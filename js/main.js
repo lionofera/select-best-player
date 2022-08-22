9be854bdb4e544ceaf0775229c4bbff4c1879566
lionofera <35335042+lionofera@users.noreply.github.com>
2022-08-22 14:25:35 +0200

    added js

---
+++ js/main.js
@@ -0,0 +1,55 @@
+document.getElementById('btn-one').addEventListener('click', function() {
+    let selectOne = document.getElementById('one').innerText;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('one').innerText;
+    document.getElementById('list').appendChild(li);
+  })
+  document.getElementById('btn-two').addEventListener('click', function() {
+    let selectOne = document.getElementById('two').innerText;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('two').innerText;
+    document.getElementById('list').appendChild(li);
+  })
+document.getElementById('btn-three').addEventListener('click', function() {
+    let selectOne = document.getElementById('three').innerText;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('three').innerText;
+    document.getElementById('list').appendChild(li);
+  })
+document.getElementById('btn-four').addEventListener('click', function() {
+    let selectOne = document.getElementById('four').innerText;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('four').innerText;
+    document.getElementById('list').appendChild(li);
+  })
+document.getElementById('btn-five').addEventListener('click', function() {
+    let selectOne = document.getElementById('five').innerText;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('five').innerText;
+    document.getElementById('list').appendChild(li);
+  })
+  document.getElementById('btn-six').addEventListener('click', function() {
+    let selectOne = document.getElementById('six').innerText;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('six').innerText;
+    document.getElementById('list').appendChild(li);
+  })
+  document.getElementById('calc').addEventListener('click', function() {
+    let playerFee = parseInt(document.getElementById('player-fee').value);
+   let numOfPlayer = document.querySelectorAll('li').length;
+   let playersFees = numOfPlayer * playerFee;
+   document.getElementById('total-playerfee').innerText = playersFees;
+  })
+  document.getElementById('calc-total').addEventListener('click', function() {
+    let managersFee = parseInt(document.getElementById('manager-fee').value);
+    let coachesFee = parseInt(document.getElementById('coach-fee').value);
+    let playersFees = parseInt(document.getElementById('total-playerfee').innerText);
+    let finalBalance = managersFee + coachesFee + playersFees;
+    document.getElementById('total').innerText = finalBalance;
+  })
