d65ef8394fae8a8d4ff9a7f6737f2def88224dc6
lionofera <35335042+lionofera@users.noreply.github.com>
2022-08-22 22:55:58 +0200

    added function to restrict listitem addition

--- js/main.js
+++ js/main.js
@@ -1,44 +1,53 @@
-
+function stopListItemCreation() {
+    if (document.getElementById('list').childNodes.length > 5) {
+        document.createElement('li') = false;
+    }
+}
 
 
 document.getElementById('btn-one').addEventListener('click', function() {
+    stopListItemCreation()
     let li = document.createElement('li');
     li.innerText = document.getElementById('one').innerText;
     document.getElementById('list').appendChild(li);
     document.getElementById('btn-one').disabled = true;
   })
   document.getElementById('btn-two').addEventListener('click', function() {
+    stopListItemCreation()
     let li = document.createElement('li');
     li.innerText = document.getElementById('two').innerText;
     document.getElementById('list').appendChild(li);
     document.getElementById('btn-two').disabled = true;
   })
 document.getElementById('btn-three').addEventListener('click', function() {
+    stopListItemCreation()
     let li = document.createElement('li');
     li.innerText = document.getElementById('three').innerText;
     document.getElementById('list').appendChild(li);
     document.getElementById('btn-three').disabled = true;
   })
 document.getElementById('btn-four').addEventListener('click', function() {
+    stopListItemCreation()
     let li = document.createElement('li');
     li.innerText = document.getElementById('four').innerText;
     document.getElementById('list').appendChild(li);
     document.getElementById('btn-four').disabled = true;
   })
 document.getElementById('btn-five').addEventListener('click', function() {
+    stopListItemCreation()
     let li = document.createElement('li');
     li.innerText = document.getElementById('five').innerText;
     document.getElementById('list').appendChild(li);
     document.getElementById('btn-five').disabled = true;
   })
   document.getElementById('btn-six').addEventListener('click', function() {
+    stopListItemCreation()
     let li = document.createElement('li');
     li.innerText = document.getElementById('six').innerText;
     document.getElementById('list').appendChild(li);
     document.getElementById('btn-six').disabled = true;
   })
 
-   
 
   document.getElementById('calc').addEventListener('click', function() {
     let playerFee = parseInt(document.getElementById('player-fee').value);
