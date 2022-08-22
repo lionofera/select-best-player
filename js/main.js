a7b91977392b6867465fe133cf247d3b3fad08c9
lionofera <35335042+lionofera@users.noreply.github.com>
2022-08-22 17:36:16 +0200

    button disabled

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+
+
+
+
+
 document.getElementById('btn-one').addEventListener('click', function() {
     let selectOne = document.getElementById('one').innerText;
     
     let li = document.createElement('li');
     li.innerText = document.getElementById('one').innerText;
     document.getElementById('list').appendChild(li);
+    document.getElementById('btn-one').disabled = true;
   })
   document.getElementById('btn-two').addEventListener('click', function() {
     let selectOne = document.getElementById('two').innerText;
@@ -11,6 +17,7 @@ document.getElementById('btn-one').addEventListener('click', function() {
     let li = document.createElement('li');
     li.innerText = document.getElementById('two').innerText;
     document.getElementById('list').appendChild(li);
+    document.getElementById('btn-two').disabled = true;
   })
 document.getElementById('btn-three').addEventListener('click', function() {
     let selectOne = document.getElementById('three').innerText;
@@ -18,6 +25,7 @@ document.getElementById('btn-three').addEventListener('click', function() {
     let li = document.createElement('li');
     li.innerText = document.getElementById('three').innerText;
     document.getElementById('list').appendChild(li);
+    document.getElementById('btn-three').disabled = true;
   })
 document.getElementById('btn-four').addEventListener('click', function() {
     let selectOne = document.getElementById('four').innerText;
@@ -25,6 +33,7 @@ document.getElementById('btn-four').addEventListener('click', function() {
     let li = document.createElement('li');
     li.innerText = document.getElementById('four').innerText;
     document.getElementById('list').appendChild(li);
+    document.getElementById('btn-four').disabled = true;
   })
 document.getElementById('btn-five').addEventListener('click', function() {
     let selectOne = document.getElementById('five').innerText;
@@ -32,6 +41,7 @@ document.getElementById('btn-five').addEventListener('click', function() {
     let li = document.createElement('li');
     li.innerText = document.getElementById('five').innerText;
     document.getElementById('list').appendChild(li);
+    document.getElementById('btn-five').disabled = true;
   })
   document.getElementById('btn-six').addEventListener('click', function() {
     let selectOne = document.getElementById('six').innerText;
@@ -39,7 +49,11 @@ document.getElementById('btn-five').addEventListener('click', function() {
     let li = document.createElement('li');
     li.innerText = document.getElementById('six').innerText;
     document.getElementById('list').appendChild(li);
+    document.getElementById('btn-six').disabled = true;
   })
+
+
+
   document.getElementById('calc').addEventListener('click', function() {
     let playerFee = parseInt(document.getElementById('player-fee').value);
    let numOfPlayer = document.querySelectorAll('li').length;
